0fb64323ed2b65d20986dcb55da07ee1c0491459
alman63 <mantulenko83@mail.ru>
2022-11-13 16:54:27 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { sum } from "./1.3";
 
 let a = prompt("Введите трехзначное число");
-while (a.length !== 3 || a.isNaN(Number(a))) {
+while (/[0-9]{3}/.test(a)) {
   a = prompt("Введите трехзначное число");
 }
 sum(a);
