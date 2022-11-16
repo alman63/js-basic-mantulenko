6bff3d31ba879281f81a62503a0f95db4c757b67
alman63 <mantulenko83@mail.ru>
2022-11-16 18:42:57 +0400

    исправил ошибку в deleteP

--- src/deleteP.js
+++ src/deleteP.js
@@ -1,7 +1,8 @@
 export function deleteP() {
-  const count = document.querySelectorAll("p").length;
+  let count = document.querySelectorAll("p").length;
   while (count > 5) {
     document.querySelectorAll("p")[0].remove();
+    count = document.querySelectorAll("p").length;
   }
   return document.querySelectorAll("p").length < 5;
 }
