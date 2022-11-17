3ae22cbea420847d539a851855a96c816d504d82
alman63 <mantulenko83@mail.ru>
2022-11-17 19:43:31 +0400

    ошибка проверки ввода

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { month } from "./2.2";
 
 let a = prompt("Введите число от 1 до 12 включительно");
-while (!/[0-9]/.test(a) || a.length !== 2 || a.length !== 1) {
+while (!/[0-9]/.test(a) || Number(a) < 0 || Number(a) > 12) {
   a = prompt("Повторите ввод. Введите число от 1 до 12 включительно !!!");
 }
 month(a);
