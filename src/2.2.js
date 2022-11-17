1d63f79c1dbd656d4df17faa1664c9b2af290d85
alman63 <mantulenko83@mail.ru>
2022-11-17 19:48:17 +0400

    ошибка вывода задачи 2.2

--- src/2.2.js
+++ src/2.2.js
@@ -2,39 +2,39 @@
 export function month(a) {
   let m = "";
   switch (a) {
-    case 1:
+    case "1":
       m = "январь";
-    case 2:
+    case "2":
       m = "февраль";
       break;
-    case 3:
+    case "3":
       m = "март";
       break;
-    case 4:
+    case "4":
       m = "апрель";
       break;
-    case 5:
+    case "5":
       m = "май";
       break;
-    case 6:
+    case "6":
       m = "июнь";
       break;
-    case 7:
+    case "7":
       m = "июль";
       break;
-    case 8:
+    case "8":
       m = "август";
       break;
-    case 9:
+    case "9":
       m = "сентябрь";
       break;
-    case 10:
+    case "10":
       m = "октябрь";
       break;
-    case 11:
+    case "11":
       m = "ноябрь";
       break;
-    case 12:
+    case "12":
       m = "декабрь";
       break;
   }
