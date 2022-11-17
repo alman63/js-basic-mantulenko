2a98b7bf676502b4df7eeb668387c166cdd8b843
alman63 <mantulenko83@mail.ru>
2022-11-17 15:48:37 +0400

    Исправил ошибки запуска обработчика

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { testInput } from "./10.1";
 
 const button = document.querySelector(".button");
-const input = document.querySelector(".text").value;
-button.addEventListener("click", testInput(input));
+
+button.addEventListener("click", () => {
+  const input = document.querySelector(".text").value;
+  testInput(input);
+});
