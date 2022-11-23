54806615e7834f736a43424371f67e19dec77ee8
alman63 <mantulenko83@mail.ru>
2022-11-23 14:06:49 +0400

    Исправил замечания после консультации

--- src/1.3.js
+++ src/1.3.js
@@ -1,6 +1,9 @@
 // функция расчета суммы цифр трехзначного числа
 export function sum(a) {
-  const result = Math.floor(a / 100) + Math.floor((a / 10) % 10) + (a % 10);
+  const result = a
+    .toString()
+    .split("")
+    .reduce((s, item) => s + Number(item), 0);
   console.log(result);
   alert(`Сумма цифр ${a} числа равна ${result}`);
   return result;
