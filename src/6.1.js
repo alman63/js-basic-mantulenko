8d41fa5d71f79723088c3836fd4950c4dee56784
alman63 <mantulenko83@mail.ru>
2022-11-24 11:53:15 +0400

    исправил замечания

--- src/6.1.js
+++ src/6.1.js
@@ -1,4 +1,4 @@
 // разница между наибольшим и наименьшим
 export function diff(a, b) {
-  return a > b ? a - b : b - a;
+  return Math.abs(a - b);
 }
--- src/6.2.js
+++ src/6.2.js
@@ -1,14 +1,5 @@
 // проерка состоит ли строка из одного слова
 export function isWord(str) {
-  const arr = str.split(" ");
-  if (arr.length === 1 && arr[0] !== "") {
-    return true;
-  }
-  if (arr.length === 3 && arr[0] === "" && arr[2] === "") {
-    return true;
-  }
-  if (arr.length === 2 && (arr[0] === " " || arr[2] === " ")) {
-    return true;
-  }
-  return false;
+  const arr = str.trim().split(" ");
+  return arr.length === 1 && arr[0] !== "";
 }
