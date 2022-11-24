62612ecf92742db2c3ff8ec643c5047c413f9f3a
alman63 <mantulenko83@mail.ru>
2022-11-24 20:21:23 +0400

    исправил замечания

--- src/8.2.js
+++ src/8.2.js
@@ -1,7 +1,7 @@
 // подсчет количества секунд с начала дня
 export function countSecond(data) {
-  console.log(
-    data.getHours() * 3600 + data.getMinutes() * 60 + data.getSeconds()
-  );
-  return data.getHours() * 3600 + data.getMinutes() * 60 + data.getSeconds();
+  const countS =
+    data.getHours() * 3600 + data.getMinutes() * 60 + data.getSeconds();
+  console.log(countS);
+  return countS;
 }
