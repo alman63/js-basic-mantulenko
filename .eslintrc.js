43e130b66a8272349ee25129dade06362825312e
alman63 <mantulenko83@mail.ru>
2023-01-23 14:39:57 +0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,34 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    "jest/globals": true,
-  },
-  extends: ["airbnb-base", "prettier"],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["jest"],
-  rules: {
-    "import/prefer-default-export": "off",
-    "consistent-return": "off",
-    "no-useless-escape": "off",
-    "no-plusplus": "off",
-    "no-console": "off",
-    "no-alert": "off",
-    "no-restricted-globals": "off",
-    "default-case": "off",
-    "no-fallthrough": "off",
-    "prefer-destructuring": ["error", { object: true, array: false }],
-    "max-len": [
-      "error",
-      {
-        ignoreComments: true,
-        ignoreStrings: true,
-      },
-    ],
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        'jest/globals': true,
+    },
+    extends: ['airbnb-base', 'prettier'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+        'import/prefer-default-export': 'off',
+        'consistent-return': 'off',
+        'no-useless-escape': 'off',
+        'no-plusplus': 'off',
+        'no-console': 'off',
+        'no-alert': 'off',
+        'no-restricted-globals': 'off',
+        'default-case': 'off',
+        'import/no-cycle': 'off',
+        'no-fallthrough': 'off',
+        'prefer-destructuring': ['error', { object: true, array: false }],
+        'max-len': [
+            'error',
+            {
+                ignoreComments: true,
+                ignoreStrings: true,
+            },
+        ],
+    },
 };
